2ffcf69a8f0c1ac392ea3e818c7c4df1cbfceab3
Jake Overall <joverall22@gmail.com>
2016-08-05 10:44:47 -0600

    what lights out >?><?<?>

--- grid.js
+++ grid.js
@@ -5,7 +5,7 @@
       template: `
         <div class="grid">
           <div class="row" ng-repeat="row in $ctrl.grid">
-            <div class="cell" ng-repeat="cell in row" ng-class="{on: cell.on}" ng-mouseOver="$ctrl.cellClick(cell)">
+            <div class="cell" ng-repeat="cell in row" ng-class="{on: cell.on}" ng-click="$ctrl.cellClick(cell)">
              {{cell.row}}, {{cell.col}}
             </div>
           </div>
@@ -30,7 +30,23 @@
       }
 
       $ctrl.cellClick = function(cell){
-        cell.on = !cell.on;
+        if(cell.row > 0){
+        let north = $ctrl.grid[cell.row-1][cell.col]
+        north.on = !north.on
+        }
+        if(cell.row < $ctrl.size-1){
+        let south = $ctrl.grid[cell.row+1][cell.col]
+        south.on = !south.on
+        }
+        if(cell.col > 0){
+        let west = $ctrl.grid[cell.row][cell.col-1]
+        west.on = !west.on
+        }
+        if(cell.col < $ctrl.size-1){
+        let east = $ctrl.grid[cell.row][cell.col+1]
+        east.on = !east.on
+        }
+        cell.on = !cell.on
       }
 
       $ctrl.$onInit = function(){
